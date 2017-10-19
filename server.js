9dad1906f8cf233a3f59c21a5eae5814d07359ed
yoruno <ryo.yoruno@gmail.com>
2017-10-19 22:34:04 +0900

    設定値を外部ファイル化

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var settings = require('./settings');
+console.log(settings);
 var server = http.createServer();
 server.on('request', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello world');
+    res.write('hello world 2nd');
     res.end();
 });
-server.listen(1337, '192.168.1.2');
+server.listen(settings.port, settings.host);
 console.log("server listening ...");
---
+++ settings.js
@@ -0,0 +1,2 @@
+exports.port = 1367;
+exports.host = '192.168.1.2';
