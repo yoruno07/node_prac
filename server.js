fe8d79c128f08e87da4943d3d9fb599cbf1d5354
yoruno <ryo.yoruno@gmail.com>
2017-10-19 23:54:26 +0900

    HTML読み込み＆表示テスト

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
 var settings = require('./settings');
 console.log(settings);
 var server = http.createServer();
 var msg;
 server.on('request', function(req, res) {
-    switch (req.url) {
-        case '/abc':
-         msg = 'this page is abc';
-         break;
-        case '/def':
-         msg = 'this page is def';
-         break;
-        default:
-         msg = 'wrong page';
-         break;
-    }
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(msg);
-    res.end();
+        fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
+                if (err) {
+                        res.writeHead(404, {'Content-Type': 'text/plain'});
+                        res.write("not found!");
+                         return res.end();
+                }
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            });
 });
 server.listen(settings.port, settings.host);
 console.log("server listening ...");
