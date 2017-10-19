c085084042a61237de723ab55a9ac5edd29e67f4
yoruno <ryo.yoruno@gmail.com>
2017-10-19 22:52:21 +0900

    URL取得テスト

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ var http = require('http');
 var settings = require('./settings');
 console.log(settings);
 var server = http.createServer();
+var msg;
 server.on('request', function(req, res) {
+    switch (req.url) {
+        case '/abc':
+         msg = 'this page is abc';
+         break;
+        case '/def':
+         msg = 'this page is def';
+         break;
+        default:
+         msg = 'wrong page';
+         break;
+    }
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello world 2nd');
+    res.write(msg);
     res.end();
 });
 server.listen(settings.port, settings.host);
