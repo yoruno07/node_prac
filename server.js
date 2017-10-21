b7858bfad35953e22556cf4bfcca8aaa9d09a1af
yoruno <ryo.yoruno@gmail.com>
2017-10-22 00:29:45 +0900

    ejsによるテンプレート読み込み

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    ejs = require('ejs');
 var settings = require('./settings');
 console.log(settings);
 var server = http.createServer();
-var msg;
+var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
+var n = 0;
 server.on('request', function(req, res) {
-        fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
-                if (err) {
-                        res.writeHead(404, {'Content-Type': 'text/plain'});
-                        res.write("not found!");
-                         return res.end();
-                }
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
-            });
+    console.log(req.url);
+      if (req.url === '/favicon.ico') {
+         return;
+      }
+    n++;
+    var data = ejs.render(template, {
+        title: "hello",
+        content: "<strong>World!</strong>",
+        n: n
+    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
 });
 server.listen(settings.port, settings.host);
 console.log("server listening ...");
